41df9ec2d5bf414a1225cca087c41afeda4a22c6
ksrinu <ksrinub06@gmail.com>
2022-01-29 08:40:33 +0530

    added docker hub data

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('ksrinub06/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('Kdevi@123')
+            registryCredentials('dockerhub')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
