524dda6832eafab60611b38f0f685bb646118fcb
ksrinu <ksrinub06@gmail.com>
2022-01-29 09:12:32 +0530

    test

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('ksrinub06/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('ksrinub06')
+            registryCredentials('dockerhub')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
