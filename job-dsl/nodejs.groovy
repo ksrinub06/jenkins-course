07bb5a7ffa69b585a61fb71f0a993ed7f279e60d
ksrinu <ksrinub06@gmail.com>
2022-01-29 09:29:34 +0530

    test

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName(''DSL User'')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigName(''ksrinub'')
+            node / gitConfigEmail('ksrinub06@gmail.com')
         }
     }
     triggers {
